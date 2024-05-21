b142a3d417668deafe9ca4cc40b5b151dd70982d
khudo <khudorimanan@gmail.com>
2024-05-21 21:30:25 +0700

    frist commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+    <div class="footer mt-4 bg-dark text-white pt-3 pb-2">
+      <div class="container">
+        Copyright <?= date('Y');?> <?= $info_web->nama_rental;?> All Reserved
+        
+      </div>
+      <div class="container">
+        
+        Alamat jl LA Sucipto 02 Blimbing Malang
+       
+      </div>
+      <div class="container">
+        Instagram: Mobil Rental 
+       
+      </div>
+      <div class="container">
+        Facebook: Rentalan
+      </div>
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
